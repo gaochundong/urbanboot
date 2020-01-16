05592f55860d53bd14adf55ef02f62fafe70dd6c
Dennis Gao <gaochundong@hotmail.com>
2020-01-16 21:03:00 +0800

    增加Swagger配置

---
+++ urban-management/urban-management-service/src/main/java/fun/goodgoodstudy/bestpractice/urbanboot/urbanmanagement/swagger/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.swagger;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    @SuppressWarnings("Guava")
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(buildApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+
+    private static ApiInfo buildApiInfo() {
+        return new ApiInfo(
+                "Urban Management Service",
+                "城市大脑服务",
+                null,
+                null,
+                null,
+                null,
+                null,
+                new ArrayList<>(0));
+    }
+}
