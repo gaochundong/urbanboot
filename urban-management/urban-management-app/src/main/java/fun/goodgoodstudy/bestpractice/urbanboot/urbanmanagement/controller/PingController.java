c336e88da1c277fdbf1924a836d6a706fd73d3da
Dennis Gao <gaochundong@hotmail.com>
2020-01-16 20:42:39 +0800

    升级依赖包，增加Swagger，增加K8S依赖，分离配置

--- urban-management/urban-management-app/src/main/java/fun/goodgoodstudy/bestpractice/urbanboot/urbanmanagement/controller/PingController.java
+++ urban-management/urban-management-app/src/main/java/fun/goodgoodstudy/bestpractice/urbanboot/urbanmanagement/controller/PingController.java
@@ -3,6 +3,7 @@ package fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -10,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
+@ApiIgnore("忽略Ping和健康检查文档")
 public class PingController {
     private static final String DATE_FORMAT_ISO8601_PATTERN = "yyyy-MM-dd'T'HH:mm:ss[.SSS'Z']";
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT_ISO8601_PATTERN);
--- urban-management/urban-management-service/src/main/java/fun/goodgoodstudy/bestpractice/urbanboot/urbanmanagement/controller/TrafficLightController.java
+++ urban-management/urban-management-service/src/main/java/fun/goodgoodstudy/bestpractice/urbanboot/urbanmanagement/controller/TrafficLightController.java
@@ -3,6 +3,8 @@ package fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.controller;
 import fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.contract.pojo.TrafficLightColor;
 import fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.exception.BadRequestException;
 import fun.goodgoodstudy.bestpractice.urbanboot.urbanmanagement.service.TrafficLightService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +20,11 @@ public class TrafficLightController {
     @Autowired
     private TrafficLightService trafficLightService;
 
+    @ApiOperation("查询城市信号灯数量")
     @GetMapping(path = "/urbanboot/cities/{cityId}/traffic-lights-count")
     public Integer getCityTrafficLightCount(
-            @PathVariable("cityId") UUID cityId,
-            @RequestParam(value = "color", required = false) String color) {
+            @PathVariable("cityId") @ApiParam(value = "城市ID") UUID cityId,
+            @RequestParam(value = "color", required = false) @ApiParam(value = "信号灯颜色") String color) {
         if (cityId == null) {
             throw new BadRequestException("城市ID不能为空");
         }
